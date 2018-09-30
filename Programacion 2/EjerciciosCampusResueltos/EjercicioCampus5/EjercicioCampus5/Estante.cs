e58e7ddb301224b5b7b3a467ce30daefe9ef34a8
Your Name <you@example.com>
2018-09-29 22:17:00 -0300

    Ejercicio  de campus falta sustraccion

---
+++ Programacion 2/EjerciciosCampusResueltos/EjercicioCampus5/EjercicioCampus5/Estante.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCampus5
+{
+    class Estante
+    {
+        private Producto[] _productos;
+        private int _ubicacionEstante;
+
+        #region Metodos
+        public Producto[] GetProductos()
+        {
+            return this._productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            int i;
+            string retorno = "";
+            retorno += e._ubicacionEstante.ToString() + "\n";
+            for (i = 0; i < e._productos.Length; i++)
+            {
+                retorno += e._productos[i].MostrarProducto(e._productos[i]);
+                retorno += "\n";
+            }
+
+            return retorno;
+        }
+
+        #region sobrecarga operadores
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool retorno = false;
+            int i;
+            for (i = 0; i < e._productos.Length; i++)
+            {
+                if (e._productos[i] == p)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+            {
+            bool retorno = false;
+            int contador = 0;
+            int i;
+            int capacidad = e._productos.Length;
+            if(e!=p)
+            {
+                for(i=0;i<capacidad;i++)
+                {
+                    if(!(Object.Equals(e._productos[i],null)))
+                    {
+                        contador++;
+                    }
+                }
+                if(contador+1<=capacidad)
+                {
+                    e._productos[contador] = p;
+                    retorno = true;
+                }
+                
+            }
+
+            return retorno;
+            }
+
+      /*  public static Estante operator -(Estante e , Producto p)
+        {
+            if(e==p)
+            {
+
+            }
+        }*/
+
+        #endregion
+        #endregion
+
+        #region Constructor
+        private Estante(int capacidad)
+        {
+            this._productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad , int ubicacion):this(capacidad)
+        {
+            this._ubicacionEstante = ubicacion;
+        }
+
+        #endregion
+    }
+}
---
+++ Programacion 2/EjerciciosCampusResueltos/EjercicioCampus5/EjercicioCampus5/Producto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCampus5
+{
+    class Producto
+    {
+        private string _codigoDeBarra;
+        private string _marca;
+        private float _precio;
+
+
+        #region Metodos
+        public string GetMarca()
+        {
+            return this._marca;
+        }
+        public float GetPrecio()
+        {
+            return this._precio;
+        }
+
+         public  string MostrarProducto(Producto p)
+        {
+            string retorno = "";
+            retorno += p._marca.ToString() + p._codigoDeBarra.ToString() + p._precio.ToString();
+            return retorno;
+        }
+        #region Sobrecarga operadores
+        public static bool operator ==(Producto p , string marca)
+        {
+            bool retorno = false;
+            if (p._marca == marca)
+                retorno = true;
+
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p , string marca)
+        {
+            return !(p == marca);
+        }
+
+        public static bool operator ==(Producto p, Producto p2)
+        {
+            bool retorno = false;
+            if (!Object.Equals(p, null) && !Object.Equals(p2, null))
+            {
+                if (p._marca == p2._marca && p._codigoDeBarra == p2._codigoDeBarra)
+                    retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p, Producto p2)
+        {
+            return !(p == p2);
+        }
+
+        public static explicit operator string (Producto p)
+        {
+            return p._codigoDeBarra;
+        }
+        #endregion
+
+        #endregion
+
+
+        #region Constructor
+        public Producto(string marca, string codigo, float precio)
+        {
+            this._codigoDeBarra = codigo;
+            this._marca = marca;
+            this._precio = precio;
+
+        }
+        #endregion
+
+    }
+}
