fccba36de9f6f1b23cda73ddcbcf1fc84cdeb321
Your Name <you@example.com>
2018-10-06 22:18:18 -0300

    primer modelo de parcial casi terminado

---
+++ Programacion 2/Parciales/Parciales Laboratorio II/pp1(no finalizado)/Villamayo.Emanuel.2A/Entidades/Jugo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   public class Jugo : Producto
+    {
+        #region Atributos
+
+        protected ESaborJugo _sabor;
+        protected bool DeConsumo;
+
+        #endregion
+
+        #region Propiedades
+
+        public override float CalcularCostoDeProduccion
+        {
+            get { return (base.Precio * 40) / 100; }
+
+        }
+
+        #endregion
+
+
+
+        #region Constructores
+        private Jugo
+
+        #endregion
+
+
+
+        public enum ESaborJugo
+        {
+            Asqueroso,
+            Pasable,
+            Rico
+        }
+
+
+    }
+}
---
+++ Programacion 2/Parciales/Parciales Laboratorio II/pp1(no finalizado)/Villamayo.Emanuel.2A/Entidades/Producto.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Producto
+    {             
+        #region Atributos
+
+        protected int _codigoBarra;
+        protected EMarcaProducto _marca;
+        protected float _precio;
+
+
+        #endregion
+
+        #region Propiedades
+        public abstract float CalcularCostoDeProduccion
+        {
+            get;
+        }
+
+        public EMarcaProducto Marca
+        {
+            get { return this._marca; }
+        }
+
+        public float Precio
+        {
+            get { return this._precio; }
+        }
+
+        #endregion
+
+
+        #region Metodos
+
+        private static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Codigo de Barras: {0}\r\n", p._codigoBarra);
+            sb.AppendFormat("MARCA: {0}\r\n", p._marca);
+            sb.AppendFormat("PRECIO: {0}\r\n", p._precio);
+            sb.AppendLine("---------------------------------");
+
+            return sb.ToString();
+
+        }
+
+        public virtual string Consumir()
+        {
+            return (string)this;
+        }
+
+        #endregion
+
+
+        #region Sobrecarga Operadores
+        public static bool operator ==(Producto p1,EMarcaProducto m)
+        {
+            bool retorno = false;
+
+            if (p1._marca == m)
+                retorno = true;
+
+            return retorno;     
+        }
+
+        public static bool operator !=(Producto p1, EMarcaProducto m)
+        {
+            return !(p1 == m);
+        }
+
+
+        public static explicit operator int (Producto p)
+        {
+            return p._codigoBarra;
+        }
+
+        public static implicit operator string(Producto p)
+        {
+            return Producto.MostrarProducto(p);
+        }
+        #endregion
+
+        public enum EMarcaProducto
+        {
+            Manaos,
+            Pitusas,
+            Naranjú,
+            Diversión,
+            Swift,
+            Favorita
+
+        }
+    }
+}
