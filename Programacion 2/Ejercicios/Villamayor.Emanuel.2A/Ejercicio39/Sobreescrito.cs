ba4f05a3f9aef36e38eb3b7b298aff83eaa68d84
Your Name <you@example.com>
2018-10-23 23:35:08 -0300

    Ejercicio 39 finalizado

---
+++ Programacion 2/Ejercicios/Villamayor.Emanuel.2A/Ejercicio39/Sobreescrito.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio39
+{
+    public abstract class Sobreescrito
+    {
+        #region Atributos
+
+        protected string miAtributo;
+
+        #endregion
+
+        #region Propiedades
+        public abstract string MiPropiedad
+        {
+            get;
+          
+        }
+   
+        #endregion
+
+        #region Constructores
+
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar abstractos";
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public override string ToString()
+        {
+            return string.Format("¡Este es mi metodo ToString sobreescrito!");
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool retorno = false;
+            if (obj is Sobreescrito)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+
+        public abstract string MiMetodo();
+
+
+
+        #endregion
+    }
+}
