7100c40f1d221a921a8912e1651c437198315fad
Your Name <you@example.com>
2018-11-10 01:53:24 -0300

    ejercicio 68 finalizado

---
+++ Programacion 2/Ejercicios/Villamayor.Emanuel.2A/Ejercicio68/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio68
+{
+    public partial class Form1 : Form
+    {
+        private Persona p;
+        private event DelegadoString evento;
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        public static void NotificarCambio(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            string nombre=textBox1.Text ;
+            string apellido=textBox2.Text;
+            if (p==null)
+            {
+                p = new Persona(textBox1.Text,textBox2.Text);
+                btnCrear.Text = "Actualizar";
+                NotificarCambio(string.Format("Se ha creado la persona "+p.Mostrar() ));
+            }
+            else
+            {
+                if(nombre==textBox1.Text && apellido==textBox2.Text)
+                {
+                    NotificarCambio(string.Format("No se ha modificado el nombre ni el apellido" + p.Mostrar()));
+                }
+                else if(nombre == textBox1.Text && apellido != textBox2.Text)
+                {
+                    NotificarCambio(string.Format("Se ha modificado solo el apellido " + p.Mostrar()));
+                    apellido = textBox2.Text;
+                }
+                else if (nombre != textBox1.Text && apellido == textBox2.Text)
+                {
+                    NotificarCambio(string.Format("Se ha modificado solo el nombre " + p.Mostrar()));
+                    nombre = textBox1.Text;
+                }
+                else
+                {
+                    NotificarCambio(string.Format("Se ha modificado  el nombre y el apellido " + p.Mostrar()));
+                    nombre = textBox1.Text;
+                    apellido = textBox2.Text;
+                }
+            }
+                                                     
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            evento += new DelegadoString(Form1.NotificarCambio);
+        }
+    }
+}
---
+++ Programacion 2/Ejercicios/Villamayor.Emanuel.2A/Ejercicio68/Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio68
+{
+    class Persona
+    {
+        #region Atributos
+
+        private string apellido;
+        private string nombre;
+
+        #endregion
+
+        #region Propiedades
+
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructores
+
+        public Persona(string nombre , string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public string Mostrar()
+        {
+            return string.Format("{0} {1}", this.Nombre, this.Apellido);
+        }
+
+        #endregion
+    }
+
+    public delegate void DelegadoString(string msg);
+}
